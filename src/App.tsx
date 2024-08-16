bdcd59c0ca5007d5e7e83f2ae637c15ee3eaa78b
Ricardo S. Ferreira <ricardo.silva_ferreira@hotmail.com>
2024-08-16 14:28:03 -0300

    CRUD feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ListaCategorias from './components/listaCategorias/ListaCategoria';
+import FormCategoria from './components/formCategoria/FormCategoria';
+import DeletarCategoria from './components/deletarCategoria/DeletarCategoria';
 import Home from './pages/home/Home';
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 
 function App() {
   return (
-    <>
-    <BrowserRouter>
-        <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
-);
+    <Router>
+      <Navbar />
+      <div className='min-h-[80vh]'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/cadastrar' element={<FormCategoria/>}/>
+          <Route path="/editarcategorias/:id" element={<FormCategoria />} />
+          <Route path="/categorias" element={<ListaCategorias />} />
+          <Route path="/categorias/editar/:id" element={<FormCategoria />} />
+          <Route path="/categorias/deletar/:id" element={<DeletarCategoria />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
 }
+
 export default App;
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 function Navbar() {
@@ -9,12 +10,18 @@ function Navbar() {
     <>
      <div className='w-full bg-blue-600 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Farmacia</div>
+            <Link to='/' className='text-2xl font-bold uppercase'>Farmacia</Link>
 
             <div className='flex gap-4'>
-              <div className='hover:underline'>Cadastrar Categorias</div>             
+            
+              <Link to='/cadastrar' className='hover:underline'>Cadastrar Categorias</Link>
+              <Link to='/categorias' className='hover:underline'>Lita de Categorias</Link>
+                         
             </div>
           </div>
+          
+              
+              
         </div>
     </>
   )
